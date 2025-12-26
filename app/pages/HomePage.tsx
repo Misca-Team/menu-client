e971c10fda2f597e321a68696f428786abbb5717
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2025-12-26 19:32:05 +0330

    🌱 add navigation to login & preview menu.

--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 function HomePage() {
   return (
-    <div>
-      <h1>صفحه اصلی</h1>
+    <div className="w-full flex justify-between px-2 py-3">
+      <Link href="/b/buna_cafe/menu"
+      className="text-sm text-white bg-brand-500 hover:bg-brand-600 cursor-pointer p-2 rounded-sm  transition-colors"
+      >منوی کافه بونا</Link>
+      <Link href="/auth/login"
+      className="text-sm text-white bg-brand-500 hover:bg-brand-600 cursor-pointer p-2 rounded-sm  transition-colors">ورود</Link>
     </div>
   );
 }
